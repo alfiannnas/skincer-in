0e16bd0dff54070063a21ceff088fa6d314d2d16
Bayu Nindioko <nbayun2@gmail.com>
2023-05-23 15:10:05 +0700

    intent camera with permission done

--- Mobile Development(App)/app/src/main/java/com/example/skincerinapp/ui/main/ScanFragment.kt
+++ Mobile Development(App)/app/src/main/java/com/example/skincerinapp/ui/main/ScanFragment.kt
@@ -3,14 +3,21 @@ package com.example.skincerinapp.ui.main
 import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.skincerinapp.R
 import com.example.skincerinapp.createCustomTempFile
 import com.example.skincerinapp.databinding.FragmentScanBinding
@@ -20,13 +27,37 @@ import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
-
 class ScanFragment : Fragment() {
 
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding!!
     private var getFile: File? = null
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().finish()
+            }
+        }
+    }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +69,20 @@ class ScanFragment : Fragment() {
         binding.checkButton.setOnClickListener {
             navigateToResultFragment()
         }
+        binding.photoButton.setOnClickListener{
+            startTakePhoto()
+        }
         binding.galleryButton.setOnClickListener{
             startGallery()
         }
 
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
         return binding.root
     }
 
@@ -49,6 +90,39 @@ class ScanFragment : Fragment() {
         val intent = Intent(requireContext(), ScanResultActivity::class.java)
         startActivity(intent)
     }
+
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(requireContext().packageManager)
+
+        createCustomTempFile(requireContext().applicationContext ).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.example.skincerinapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == AppCompatActivity.RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+
+            myFile.let { file ->
+
+                getFile = file
+                binding.imageBanner.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
 
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
